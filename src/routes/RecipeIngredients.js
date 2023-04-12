a6fd2e091735847293d5eb0a0943f686dc643921
Hunter Storm <huntersreese@gmail.com>
2023-04-12 00:41:19 -0600

    added a get recipe ingredients by recipe id function

--- src/routes/RecipeIngredients.js
+++ src/routes/RecipeIngredients.js
@@ -4,6 +4,8 @@
 const express = require('express');
 const router = express.Router() 
 const RecipeIngredient = require('../models/RecipeIngredient');
+const Recipe = require('../models/Recipe');
+const Ingredient = require('../models/Ingredient');
 
 //get all recipe ingredients
 router.get('/',(req,res)=>{
@@ -15,6 +17,28 @@ router.get('/',(req,res)=>{
         res.status(200);
     })
 })
+
+// get recipe ingredients by recipe ID
+router.get('/recipe/:recipe_id', (req, res) => {
+    const id = req.params.recipe_id;
+  
+    Recipe.findByPk(id, {
+      include: [{
+        model: Ingredient,
+        through: RecipeIngredient,
+        attributes: [ 'ingredientName', 'ingredientType']
+      }]
+    })
+    .then(recipe => {
+      if (!recipe) {
+        return res.status(404).json({ error: 'Recipe not found' });
+      }
+      res.status(200).json(recipe.ingredients);
+    })
+    .catch(error => {
+      res.status(500).send(`Error getting recipe: ${error.message}`);
+    });
+  });
 
 //export
 module.exports = router;
