943e5e9f589ab7523245db7a8d156cb86a8acc94
Hunter Storm <huntersreese@gmail.com>
2023-04-24 20:34:37 -0600

    added post function for S3 image bucket

--- src/routes/RetrieveImage.js
+++ src/routes/RetrieveImage.js
@@ -7,11 +7,11 @@ const AWS = require('aws-sdk');
 // configure AWS SDK
 AWS.config.update({
   accessKeyId: 'AKIATYDAXPPCTH6XDZNR',
-  secretAccessKey: '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'
+  secretAccessKey: 'tXFSUv8yyx3e4p3JVdx58TPcN1w/7oGRL9j7GKmZ',
+  region: 'us-west-2' 
 });
 
 // create S3 client
-AWS.config.update({ region: 'us-west-2' });
 const s3 = new AWS.S3();
 
 // endpoint to retrieve image from S3 bucket
@@ -29,6 +29,33 @@ router.get('/:key', (req, res) => {
       res.setHeader('Content-Type', data.ContentType);
     res.send(data.Body);
   }
+  });
+});
+
+// endpoint to upload image to S3 bucket
+router.post('/upload', (req, res) => {
+  if (!req.files || !req.files.file) {
+    res.status(400).send('No file uploaded');
+    console.log(req.files)
+    return;
+  }
+
+  const file = req.files.file;
+  const key =  file.name;
+  const params = {
+    Bucket: 'traderrecipeimages',
+    Key: key,
+    Body: file.data,
+    ContentType: file.mimetype
+  };
+
+  s3.putObject(params, (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error uploading image');
+    } else {
+      res.send('Image uploaded successfully');
+    }
   });
 });
 
--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ const http = require('http');
 const express = require ('express')
 const app = express();
 const sequelize = require('./sequelizeConnection');
+const fileUpload = require('express-fileupload');
 const cors = require('cors');
 
 // router variables
@@ -26,6 +27,10 @@ const Favorite = require('./models/Favorite');
 // middleware functions
 app.use(express.json());
 app.use(cors());
+app.use(fileUpload({
+    limits: { fileSize: 50 * 1024 * 1024 } // set the maximum file size to 50MB
+  }));
+  
 
 //sequelize routes
 app.use('/Users', userRoute);
@@ -76,7 +81,11 @@ sequelize.sync().then(()=>{
     console.log("tables created successfully");
 })
 
+// const server = http.createServer(app);
+// server.listen(8008, '0.0.0.0', ()=>{
+//     console.log('server started');
+// })
 const server = http.createServer(app);
-server.listen(8008, '0.0.0.0', ()=>{
+server.listen(3000, '127.0.0.1', ()=>{
     console.log('server started');
 })
