424de591cf6a90f1058317925e29e1ce8aa6bb5e
Hunter Storm <huntersreese@gmail.com>
2023-04-30 22:34:34 -0600

    trying desperately to get this image to render

--- src/routes/RetrieveImage.js
+++ src/routes/RetrieveImage.js
@@ -2,77 +2,39 @@ const express = require('express');
 const router = express.Router();
 const AWS = require('aws-sdk');
 
-
-
 // configure AWS SDK
 AWS.config.update({
   accessKeyId: 'AKIATYDAXPPCTH6XDZNR',
-  secretAccessKey: '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',
-  region: 'us-west-2' 
+  secretAccessKey: 'YtXFSUv8yyx3e4p3JVdx58TPcN1w/7oGRL9j7GKmZ',
+  region: 'us-west-2'
 });
 
 // create S3 client
 const s3 = new AWS.S3();
 
 // endpoint to retrieve image from S3 bucket
-// router.get('/:key', (req, res) => {
-//   const params = {
-//     Bucket: 'traderrecipeimages',
-//     Key: req.params.key // use the key from the URL params
-//   };
-
-//   s3.getObject(params, (err, data) => {
-//     if (err) {
-//       console.error(err);
-//       res.status(500).send('Error retrieving image');
-//     } else {
-//       res.setHeader('Content-Type', data.ContentType);
-//     res.send(data.Body);
-//   }
-//   });
-// });
-
-// router.get('/:key', (req, res) => {
-//   const params = {
-//     Bucket: 'traderrecipeimages',
-//     Key: req.params.key // use the key from the URL params
-//   };
-
-//   s3.getObject(params, (err, data) => {
-//     if (err) {
-//       console.error(err);
-//       res.status(500).send('Error retrieving image');
-//     } else {
-      
-//       const url = s3.getSignedUrl('getObject', params);
-//       res.send(url);
-//     }
-//   });
-// });
-
 router.get('/:key', (req, res) => {
-  res.setHeader('Cache-Control', 'no-cache');
   const params = {
     Bucket: 'traderrecipeimages',
     Key: req.params.key // use the key from the URL params
   };
 
-  s3.getObject(params, (err, data) => {
-    if (err) {
-      console.error(err);
-      res.status(500).send('Error retrieving image');
-    } else {
-      res.setHeader('Content-Type', data.ContentType);
-    res.send(data.Body);
-  }
+  // generate a presigned URL for the image with a 10 expiration time
+  const url = s3.getSignedUrl('getObject', {
+    Bucket: params.Bucket,
+    Key: params.Key,
+    Expires: 315360000  
   });
+
+  // redirect the user to the presigned URL to retrieve the image
+  res.redirect(url);
 });
 
 // endpoint to upload image to S3 bucket
 router.post('/upload', (req, res) => {
   if (!req.files || !req.files.file) {
     res.status(400).send('No file uploaded');
-    console.log(req.files)
+    console.log(req.files);
     return;
   }
 
