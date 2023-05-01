ceda729d2acdecb33c97240e7d15639e3c479da2
Hunter Storm <huntersreese@gmail.com>
2023-04-30 20:40:41 -0600

    added image URL for front end

--- src/routes/RetrieveImage.js
+++ src/routes/RetrieveImage.js
@@ -15,6 +15,23 @@ AWS.config.update({
 const s3 = new AWS.S3();
 
 // endpoint to retrieve image from S3 bucket
+// router.get('/:key', (req, res) => {
+//   const params = {
+//     Bucket: 'traderrecipeimages',
+//     Key: req.params.key // use the key from the URL params
+//   };
+
+//   s3.getObject(params, (err, data) => {
+//     if (err) {
+//       console.error(err);
+//       res.status(500).send('Error retrieving image');
+//     } else {
+//       res.setHeader('Content-Type', data.ContentType);
+//     res.send(data.Body);
+//   }
+//   });
+// });
+
 router.get('/:key', (req, res) => {
   const params = {
     Bucket: 'traderrecipeimages',
@@ -26,9 +43,10 @@ router.get('/:key', (req, res) => {
       console.error(err);
       res.status(500).send('Error retrieving image');
     } else {
-      res.setHeader('Content-Type', data.ContentType);
-    res.send(data.Body);
-  }
+      const imageBuffer = data.Body;
+      const dataUrl = `data:${data.ContentType};base64,${imageBuffer.toString('base64')}`;
+      res.send(dataUrl);
+    }
   });
 });
 
